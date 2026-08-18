cc764a8c83e12848e7dcb30d86b23a945565dbe7
GenXinvenits <genxinvenits@outlook.com>
2026-08-18 16:41:17 +0530

    Refine PrivacyVista sidebar branding

--- app/Views/layouts/sidebar.php
+++ app/Views/layouts/sidebar.php
@@ -1,46 +1,23 @@
 <aside class="sidebar d-flex flex-column">
-    <div class="sidebar-brand">
-        <img src="https://privacyvista.com/wp-content/uploads/2025/12/privacy-vista-logo-light.png" alt="PrivacyVista">
-        <span>PrivacyVista</span>
+    <div class="sidebar-brand justify-content-center">
+        <a href="/app/public/index.php?route=dashboard" aria-label="PrivacyVista Dashboard">
+            <img src="https://privacyvista.com/wp-content/uploads/2025/12/privacy-vista-logo-light.png" alt="PrivacyVista">
+        </a>
     </div>
 
     <nav class="nav flex-column py-2">
         <div class="sidebar-section">Workspace</div>
 
-        <a class="nav-link" href="/app/public/index.php?route=dashboard">
-            <svg fill="none" viewBox="0 0 24 24" stroke-width="1.8"><path stroke-linecap="round" stroke-linejoin="round" d="M3.75 3.75h6.5v6.5h-6.5zM13.75 3.75h6.5v6.5h-6.5zM3.75 13.75h6.5v6.5h-6.5zM13.75 13.75h6.5v6.5h-6.5z"/></svg>
-            <span>Dashboard</span>
-        </a>
-        <a class="nav-link" href="/app/public/index.php?route=clients">
-            <svg fill="none" viewBox="0 0 24 24" stroke-width="1.8"><path stroke-linecap="round" stroke-linejoin="round" d="M16 21v-2a4 4 0 0 0-4-4H6a4 4 0 0 0-4 4v2M9 11a4 4 0 1 0 0-8 4 4 0 0 0 0 8zM22 21v-2a4 4 0 0 0-3-3.87M16 3.13a4 4 0 0 1 0 7.75"/></svg>
-            <span>Clients</span>
-        </a>
-        <a class="nav-link" href="/app/public/index.php?route=processing-activities">
-            <svg fill="none" viewBox="0 0 24 24" stroke-width="1.8"><path stroke-linecap="round" stroke-linejoin="round" d="M8 6h13M8 12h13M8 18h13M3 6h.01M3 12h.01M3 18h.01"/></svg>
-            <span>Processing Activities</span>
-        </a>
-        <a class="nav-link" href="/app/public/index.php?route=assessments">
-            <svg fill="none" viewBox="0 0 24 24" stroke-width="1.8"><path stroke-linecap="round" stroke-linejoin="round" d="M9 11l3 3L22 4M21 12v7a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V5a2 2 0 0 1 2-2h11"/></svg>
-            <span>Assessments</span>
-        </a>
-        <a class="nav-link" href="/app/public/index.php?route=findings">
-            <svg fill="none" viewBox="0 0 24 24" stroke-width="1.8"><path stroke-linecap="round" stroke-linejoin="round" d="M12 9v4M12 17h.01M10.3 3.86l-8 14A2 2 0 0 0 4 21h16a2 2 0 0 0 1.74-3l-8-14a2 2 0 0 0-3.48 0z"/></svg>
-            <span>Findings</span>
-        </a>
-        <a class="nav-link" href="/app/public/index.php?route=tasks">
-            <svg fill="none" viewBox="0 0 24 24" stroke-width="1.8"><path stroke-linecap="round" stroke-linejoin="round" d="M9 11l3 3L22 4M21 12v7a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V5a2 2 0 0 1 2-2h11"/></svg>
-            <span>Remediation Tasks</span>
-        </a>
+        <a class="nav-link" href="/app/public/index.php?route=dashboard"><span>Dashboard</span></a>
+        <a class="nav-link" href="/app/public/index.php?route=clients"><span>Clients</span></a>
+        <a class="nav-link" href="/app/public/index.php?route=processing-activities"><span>Processing Activities</span></a>
+        <a class="nav-link" href="/app/public/index.php?route=assessments"><span>Assessments</span></a>
+        <a class="nav-link" href="/app/public/index.php?route=findings"><span>Findings</span></a>
+        <a class="nav-link" href="/app/public/index.php?route=tasks"><span>Remediation Tasks</span></a>
 
         <div class="sidebar-section">Administration</div>
-        <a class="nav-link" href="/app/public/index.php?route=users">
-            <svg fill="none" viewBox="0 0 24 24" stroke-width="1.8"><path stroke-linecap="round" stroke-linejoin="round" d="M16 21v-2a4 4 0 0 0-4-4H6a4 4 0 0 0-4 4v2M9 11a4 4 0 1 0 0-8 4 4 0 0 0 0 8zM22 21v-2a4 4 0 0 0-3-3.87M16 3.13a4 4 0 0 1 0 7.75"/></svg>
-            <span>Users</span>
-        </a>
-        <a class="nav-link" href="/app/public/index.php?route=departments">
-            <svg fill="none" viewBox="0 0 24 24" stroke-width="1.8"><path stroke-linecap="round" stroke-linejoin="round" d="M3 21h18M5 21V7l7-4 7 4v14M9 21v-5h6v5M9 10h.01M15 10h.01"/></svg>
-            <span>Departments</span>
-        </a>
+        <a class="nav-link" href="/app/public/index.php?route=users"><span>Users</span></a>
+        <a class="nav-link" href="/app/public/index.php?route=departments"><span>Departments</span></a>
 
         <div class="sidebar-section">More</div>
         <a class="nav-link disabled" href="#" aria-disabled="true" tabindex="-1"><span>Forms</span></a>
@@ -49,9 +26,6 @@
     </nav>
 
     <div class="mt-auto p-2">
-        <a class="nav-link text-danger" href="/app/public/index.php?route=logout">
-            <svg fill="none" viewBox="0 0 24 24" stroke-width="1.8"><path stroke-linecap="round" stroke-linejoin="round" d="M10 17l5-5-5-5M15 12H3M21 19V5a2 2 0 0 0-2-2h-6"/></svg>
-            <span>Logout</span>
-        </a>
+        <a class="nav-link text-danger" href="/app/public/index.php?route=logout"><span>Logout</span></a>
     </div>
 </aside>
