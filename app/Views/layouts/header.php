b7d22759085bcaa039b8841cbc5e7d2972568d33
GenXinvenits <genxinvenits@outlook.com>
2026-08-18 18:46:07 +0530

    Load Reports stylesheet

--- app/Views/layouts/header.php
+++ app/Views/layouts/header.php
@@ -7,6 +7,7 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css" rel="stylesheet">
     <link rel="stylesheet" href="/app/public/assets/css/style.css?v=2">
     <link rel="stylesheet" href="/app/public/assets/css/forms.css?v=1">
+    <link rel="stylesheet" href="/app/public/assets/css/reports.css?v=1">
 </head>
 <body>
 <div class="app-shell">
