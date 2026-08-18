08493a8af0e18826694bb64d3d4b0e24eb3d196c
GenXinvenits <genxinvenits@outlook.com>
2026-08-18 13:18:47 +0530

    Require POST for destructive CRUD routes

--- routes/web.php
+++ routes/web.php
@@ -11,14 +11,14 @@ $router->get('users/create', ['UsersController', 'create']);
 $router->post('users/store', ['UsersController', 'store']);
 $router->get('users/edit', ['UsersController', 'edit']);
 $router->post('users/update', ['UsersController', 'update']);
-$router->get('users/delete', ['UsersController', 'delete']);
+$router->post('users/delete', ['UsersController', 'delete']);
 
 $router->get('clients', ['ClientsController', 'index']);
 $router->get('clients/create', ['ClientsController', 'create']);
 $router->post('clients/store', ['ClientsController', 'store']);
 $router->get('clients/edit', ['ClientsController', 'edit']);
 $router->post('clients/update', ['ClientsController', 'update']);
-$router->get('clients/delete', ['ClientsController', 'delete']);
+$router->post('clients/delete', ['ClientsController', 'delete']);
 $router->get('clients/show', ['ClientsController', 'show']);
 
 $router->get('departments', ['DepartmentsController', 'index']);
@@ -26,7 +26,7 @@ $router->get('departments/create', ['DepartmentsController', 'create']);
 $router->post('departments/store', ['DepartmentsController', 'store']);
 $router->get('departments/edit', ['DepartmentsController', 'edit']);
 $router->post('departments/update', ['DepartmentsController', 'update']);
-$router->get('departments/delete', ['DepartmentsController', 'delete']);
+$router->post('departments/delete', ['DepartmentsController', 'delete']);
 
 $router->get('processing-activities', ['ProcessingActivitiesController', 'index']);
 $router->get('processing-activities/create', ['ProcessingActivitiesController', 'create']);
