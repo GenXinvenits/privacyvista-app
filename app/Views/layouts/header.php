a388f79d808c0d19e613f2be44d9b56b6c1c1e9e
GenXinvenits <genxinvenits@outlook.com>
2026-08-19 03:10:29 +0530

    Load enhanced iOS-inspired liquid glass

--- app/Views/layouts/header.php
+++ app/Views/layouts/header.php
@@ -22,6 +22,7 @@
     <link rel="stylesheet" href="/app/public/assets/css/liquid-glass.css?v=1">
     <link rel="stylesheet" href="/app/public/assets/css/liquid-glass-theme.css?v=1">
     <link rel="stylesheet" href="/app/public/assets/css/liquid-glass-fixes.css?v=1">
+    <link rel="stylesheet" href="/app/public/assets/css/liquid-glass-ios27.css?v=1">
     <script src="/app/public/assets/js/theme.js?v=1" defer></script>
 </head>
 <body>
