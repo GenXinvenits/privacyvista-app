cc11fb62da6133d337d362a512a43931572b5b29
GenXinvenits <genxinvenits@outlook.com>
2026-08-18 18:45:31 +0530

    Add Reports route

--- routes/web.php
+++ routes/web.php
@@ -8,4 +8,5 @@ $router->get('assessments', ['AssessmentsController', 'index']); $router->get('a
 $router->get('findings', ['FindingsController', 'index']); $router->get('findings/create', ['FindingsController', 'create']); $router->post('findings/store', ['FindingsController', 'store']); $router->post('findings/status', ['FindingsController', 'status']);
 $router->get('tasks', ['TasksController', 'index']); $router->get('tasks/create', ['TasksController', 'create']); $router->post('tasks/store', ['TasksController', 'store']); $router->post('tasks/status', ['TasksController', 'status']);
 $router->get('forms', ['FormsController', 'index']);
+$router->get('reports', ['ReportsController', 'index']);
 $router->get('privacy-dashboard', ['PrivacyDashboardController', 'show']);
