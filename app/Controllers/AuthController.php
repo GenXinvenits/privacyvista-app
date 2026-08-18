49e505fefb63cb2812dc69b77af347a064a3af7b
GenXinvenits <genxinvenits@outlook.com>
2026-08-18 16:41:24 +0530

    Ensure logout clears authentication session

--- app/Controllers/AuthController.php
+++ app/Controllers/AuthController.php
@@ -28,7 +28,6 @@ class AuthController extends Controller
         ");
 
         $stmt->execute([$email]);
-
         $user = $stmt->fetch();
 
         if (!$user || !password_verify($password, $user['password'])) {
@@ -37,6 +36,7 @@ class AuthController extends Controller
             exit;
         }
 
+        session_regenerate_id(true);
         $_SESSION['user'] = [
             'id' => $user['id'],
             'fullname' => $user['fullname'],
@@ -50,7 +50,15 @@ class AuthController extends Controller
 
     public function logout()
     {
+        $_SESSION = [];
+
+        if (ini_get('session.use_cookies')) {
+            $params = session_get_cookie_params();
+            setcookie(session_name(), '', time() - 42000, $params['path'], $params['domain'], $params['secure'], $params['httponly']);
+        }
+
         session_destroy();
+
         header('Location: /app/public/index.php?route=login');
         exit;
     }
