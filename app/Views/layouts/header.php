1c0b9c9ba2a389b802ef73b1085207205a5a29f1
GenXinvenits <genxinvenits@outlook.com>
2026-08-19 03:03:54 +0530

    Load liquid glass UI fixes

--- app/Views/layouts/header.php
+++ app/Views/layouts/header.php
@@ -21,6 +21,7 @@
     <link rel="stylesheet" href="/app/public/assets/css/settings.css?v=1">
     <link rel="stylesheet" href="/app/public/assets/css/liquid-glass.css?v=1">
     <link rel="stylesheet" href="/app/public/assets/css/liquid-glass-theme.css?v=1">
+    <link rel="stylesheet" href="/app/public/assets/css/liquid-glass-fixes.css?v=1">
     <script src="/app/public/assets/js/theme.js?v=1" defer></script>
 </head>
 <body>
