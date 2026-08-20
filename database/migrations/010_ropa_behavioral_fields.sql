49318d498cda40f23cc7581b8d2f41e28d30545c
GenXinvenits <genxinvenits@outlook.com>
2026-08-20 22:19:57 +0530

    Correct Processor ROPA v3 derived review date

--- database/migrations/010_ropa_behavioral_fields.sql
+++ database/migrations/010_ropa_behavioral_fields.sql
@@ -38,9 +38,9 @@ SELECT ft.id, 3,
          '$.sections[4].fields[2].show_when', JSON_OBJECT('field','dpia_applicability','equals','yes'),
          '$.sections[4].fields[13].show_when', JSON_OBJECT('field','dpia_applicability','equals','yes'),
          '$.sections[4].fields[14].show_when', JSON_OBJECT('field','adm_profiling','equals','yes'),
-         '$.sections[4].fields[11].type', 'derived_date',
-         '$.sections[4].fields[11].derived_from', 'last_audit_date',
-         '$.sections[4].fields[11].offset_months', 11
+         '$.sections[4].fields[9].type', 'derived_date',
+         '$.sections[4].fields[9].derived_from', 'last_audit_date',
+         '$.sections[4].fields[9].offset_months', 11
        ),
        'published', 'Add Processor conditional, automated and derived field behavior', NULL
 FROM form_templates ft
@@ -48,15 +48,9 @@ JOIN form_template_versions v ON v.form_template_id=ft.id AND v.version_number=2
 WHERE ft.slug='ropa-processor'
   AND NOT EXISTS (SELECT 1 FROM form_template_versions x WHERE x.form_template_id=ft.id AND x.version_number=3);
 
--- Add the Processor ADM detail field after v3 creation so its condition can reference the new ADM selector.
 UPDATE form_template_versions v
 JOIN form_templates ft ON ft.id=v.form_template_id
-SET v.definition = JSON_ARRAY_APPEND(
-    JSON_SET(v.definition,
-      '$.sections[4].fields[15].show_when', JSON_OBJECT('field','adm_profiling','equals','yes')
-    ),
-    '$.sections[4].fields', JSON_OBJECT('key','adm_profiling_details','label','ADM / Profiling Details','type','textarea','show_when',JSON_OBJECT('field','adm_profiling','equals','yes'))
-)
+SET v.definition = JSON_ARRAY_APPEND(v.definition,'$.sections[4].fields',JSON_OBJECT('key','adm_profiling_details','label','ADM / Profiling Details','type','textarea','show_when',JSON_OBJECT('field','adm_profiling','equals','yes')))
 WHERE ft.slug='ropa-processor'
   AND v.version_number=3
   AND v.status='published';
